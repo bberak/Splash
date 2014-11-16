81eafcb706e0e67e6a3da600551c394848e5d2af
bberak <bberak@gmail.com>
2014-11-16 15:50:47 +1100

    Using gulp-connect and opn instead of gulp-webserver because it appears to be more performant and reliable

--- Site/gulpfile.js
+++ Site/gulpfile.js
@@ -1,10 +1,11 @@
 var gulp        = require('gulp');
 var plumber     = require('gulp-plumber');
 var sass        = require('gulp-sass');
-var webserver   = require('gulp-webserver');
+var connect     = require('gulp-connect');
 var browserify  = require('gulp-browserify');
 var clean       = require('gulp-clean');
 var runSequence = require('run-sequence');
+var opn         = require('opn');
 
 var config = {
   src: {
@@ -26,7 +27,7 @@ var config = {
   cleanPaths: ['build/*'],
   server: {
     host: 'localhost',
-    port: '8001'
+    port: 8001
   }
 };
 
@@ -40,7 +41,8 @@ gulp.task('styles', function () {
   gulp.src(config.src.styles)
     .pipe(plumber())
     .pipe(sass())
-    .pipe(gulp.dest(config.dist.styles));
+    .pipe(gulp.dest(config.dist.styles))
+    .pipe(connect.reload());
 });
 
 gulp.task('scripts', function() {
@@ -53,28 +55,21 @@ gulp.task('scripts', function() {
         paths: config.browserify.modulePaths
       }))
       .pipe(gulp.dest(config.dist.scripts))
+      .pipe(connect.reload());
 });
 
 gulp.task('images', function() {
   return gulp.src(config.src.images)
     .pipe(plumber())
-    .pipe(gulp.dest(config.dist.images));
+    .pipe(gulp.dest(config.dist.images))
+    .pipe(connect.reload());
 });
 
 gulp.task('html', function() {
   return gulp.src(config.src.html)
     .pipe(plumber())
-    .pipe(gulp.dest(config.dist.html));
-});
-
-gulp.task('webserver', function() {
-  gulp.src(config.dist.html)
-    .pipe(webserver({
-      host:         config.server.host,
-      port:         config.server.port,
-      livereload:   true,
-      open:         true
-    }));
+    .pipe(gulp.dest(config.dist.html))
+    .pipe(connect.reload());
 });
 
 gulp.task('watch', function(){
@@ -84,8 +79,21 @@ gulp.task('watch', function(){
   gulp.watch(config.src.html,    ['html']);
 });
 
+gulp.task('server', function() {
+  connect.server({
+    root: config.dist.html,
+    host: config.server.host,
+    port: config.server.port,
+    livereload: true
+  });
+});
+
+gulp.task('browser', function() {
+  opn('http://' + config.server.host + ":" + config.server.port);
+});
+
 gulp.task('build', function(callback) {
-  runSequence('clean', ['styles', 'scripts', 'images', 'html'], 'watch', 'webserver', callback);
+  runSequence('clean', ['styles', 'scripts', 'images', 'html'], 'watch', 'server', 'browser', callback);
 });
 
 gulp.task('default', ['build']);
